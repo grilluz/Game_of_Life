d8dfdf70689bb5c4facd3775264e733fc8da462a
grilluz <andrea.grillo96@gmail.com>
2016-05-24 18:20:52 +0200

    add license

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,3 +1,21 @@
+/*
+Simple implementation of "Game of Life" in C!
+Copyright (C) 2016  Grilluz
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "game_of_life.h"
 //compute live cells destiny:
 //if it has 2-3 neighbours it survives...
